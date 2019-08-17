a2f7fa8ef3ffdb221f9e15720de4f45a61c17425
saikrish17 <51330088+saikrish17@users.noreply.github.com>
2019-08-17 14:54:29 +0530

    Tracking of next File

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ def addone(num):
 def square(num):
     return num**2
 
+def cube(num):
+    return num**3
+
 if __name__ == "__main__":
     print(addone(5))
     print(square(5))
+    print(cube(5))
